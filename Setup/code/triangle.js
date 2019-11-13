a119e54b8c10a2884b5d39f2c8e0fe39e2d2c62d
le-phuong-anh <lephuonganh4995@gmail.com>
2019-11-13 21:25:25 +0200

    Triangle exercise solution

---
+++ Setup/code/triangle.js
@@ -0,0 +1,25 @@
+// 1. Looping a triangle
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// ```js
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// ```
+
+// After this exercise, you should be familiar with loop, string's methods
+// > Hint: after every loop, add one more `#` to your string and console.log()
+
+
+
+function triangleBuilding() {
+    var string = "#";
+    var repeatedString = "";
+    for (var i = 0; i < 7; i++) {
+        repeatedString = repeatedString + string;
+        console.log(repeatedString);
+    }
+}
