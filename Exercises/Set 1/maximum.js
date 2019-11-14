b4aec1a5e1d47aa4753e4d1ed7ee5cc3a2e02eb3
le-phuong-anh <lephuonganh4995@gmail.com>
2019-11-14 23:44:12 +0200

    solutions to exercise set 1

---
+++ Exercises/Set 1/maximum.js
@@ -0,0 +1,25 @@
+// 3. Maximum
+// Math.max returns its largest argument. We can build something like that now. Write a function findMax that takes three arguments and returns their maxiumum. Without method Math.max method.
+// ```js
+// console.log(findMax(0, 10, 5));
+// 10
+// console.log(findMax(0, -10,-2));
+// 0
+// ```
+
+
+function findMax(x,y,z) {
+    if (x > y && x > z) {
+        console.log(x);
+    } else if (y > x && y > z) {
+        console.log(y);
+    } else if (z > x && z > y) {
+        console.log(z); 
+    } else if (x = y = z) {
+        console.log(x);
+    } else {
+        console.log();
+    }
+}
+
+//What if we have an array of number, rather than just 3 integer numbers? 
