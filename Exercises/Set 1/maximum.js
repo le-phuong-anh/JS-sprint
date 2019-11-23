6f8c91b8683ef672eaa6350d7ad1b22462305405
lelevan3010 <93vannd@gmail.com>
2019-11-24 00:18:27 +0200

    solution to exercises

--- Exercises/Set 1/maximum.js
+++ Exercises/Set 1/maximum.js
@@ -7,14 +7,13 @@
 // 0
 // ```
 
-
 // function findMax(x,y,z) {
 //     if (x > y && x > z) {
 //         console.log(x);
 //     } else if (y > x && y > z) {
 //         console.log(y);
 //     } else if (z > x && z > y) {
-//         console.log(z); 
+//         console.log(z);
 //     } else if (x = y = z) {
 //         console.log(x);
 //     } else {
@@ -22,13 +21,25 @@
 //     }
 // }
 
-//What if we have an array of number, rather than just 3 integer numbers? 
+//What if we have an array of number, rather than just 3 integer numbers?
 
-function anotherFindMax(x, ...args){
-    if (x > args) {
-        console.log(x);
+function anotherFindMax(...args) {
+  //args is an object
+  //   if (x > args) {
+  //     console.log(x);
+  //   }
+  console.log("args", args);
+  console.log("typeof:", typeof args);
+  let val = args[0];
+  for (i = 1; i <= args.length; i++) {
+    if (val < args[i]) {
+      val = args[i];
     }
+  }
+  console.log(val);
 }
+
+anotherFindMax(1, 4, 67, 7, 31, 13);
 
 //i'm still thinking how to use the rest parameter
 //another direction i was thinking of is to create a ray of number, and then use the loop function to check if a number in the array is bigger than any other number, then we return that number
